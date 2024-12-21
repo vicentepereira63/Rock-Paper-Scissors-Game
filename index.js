cf493ece20cc7b5ec38848557e70e6d18e4a622f
vicentepereira63 <147882117+vicentepereira63@users.noreply.github.com>
2024-12-21 22:48:07 +0000

    Adding a Function to the decisions that the human and the computer do

--- index.js
+++ index.js
@@ -9,11 +9,38 @@ function getComputerChoice() {
   }
   return computerChoice;
 }
-
 function getHumanChoice() {
   humanChoice = prompt("Rock,Paper or Scissors?");
+  humanChoice = humanChoice.toUppercase(0) + humanChoice.toLowercase(0);
   return humanChoice;
 }
+function playround(humanChoice, computerChoice) {
+  humanChoice = getHumanChoice();
+  computerChoice = getComputerChoice();
+  console.log("You've Choosed" + humanChoice);
+  console.log("The computer choose" + computerChoice);
+  if (humanChoice == "Paper" && computerChoice == "Scissors") {
+    console.log("You've Lost! Scissors beat Paper");
+    computerScore = computerScore + 1;
+  } else if (humanChoice == "Rock" && computerChoice == "Paper") {
+    console.log("You've Lost! Paper beat Rock");
+    computerScore = computerScore + 1;
+  } else if (humanChoice == "Scissors" && computerChoice == "Rock") {
+    console.log("You've Lost! Scissors beat Paper");
+    computerScore = computerScore + 1;
+  } else if (humanChoice == "Scissors" && computerChoice == "Paper") {
+    console.log("You've Won! Scissors beat Paper");
+    humanScore = humanScore + 1;
+  } else if (humanChoice == "Rock" && computerChoice == "Scissors") {
+    console.log("You've Won! Rock beat Scissors");
+    computerScore = computerScore + 1;
+  } else if (humanChoice == "Paper" && computerChoice == "Rock") {
+    console.log("You've Won! Paper beat Rock");
+    computerScore = computerScore + 1;
+  } else {
+    console.log("It was a Tie! Play again");
+  }
+}
 
-let humanScore = 0;
-let computerScore = 0;
+const humanScore = 0;
+const computerScore = 0;

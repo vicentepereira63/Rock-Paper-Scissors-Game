9eacce2365358a769d52514e8f020dcadc7d4ea4
vicentepereira63 <147882117+vicentepereira63@users.noreply.github.com>
2024-12-21 23:32:03 +0000

    Adding Function to play the game with a scoreboard included

--- index.js
+++ index.js
@@ -11,32 +11,31 @@ function getComputerChoice() {
 }
 function getHumanChoice() {
   humanChoice = prompt("Rock,Paper or Scissors?");
-  humanChoice = humanChoice.toUppercase(0) + humanChoice.toLowercase(0);
   return humanChoice;
 }
-function playround(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice) {
   humanChoice = getHumanChoice();
   computerChoice = getComputerChoice();
-  console.log("You've Choosed" + humanChoice);
-  console.log("The computer choose" + computerChoice);
+  console.log("You've Choosed " + humanChoice);
+  console.log("The computer choosed " + computerChoice);
   if (humanChoice == "Paper" && computerChoice == "Scissors") {
-    console.log("You've Lost! Scissors beat Paper");
+    console.log("You've Lost! Scissors beats Paper");
     computerScore = computerScore + 1;
   } else if (humanChoice == "Rock" && computerChoice == "Paper") {
-    console.log("You've Lost! Paper beat Rock");
+    console.log("You've Lost! Paper beats Rock");
     computerScore = computerScore + 1;
   } else if (humanChoice == "Scissors" && computerChoice == "Rock") {
-    console.log("You've Lost! Scissors beat Paper");
+    console.log("You've Lost! Scissors beats Paper");
     computerScore = computerScore + 1;
   } else if (humanChoice == "Scissors" && computerChoice == "Paper") {
-    console.log("You've Won! Scissors beat Paper");
+    console.log("You've Won! Scissors beats Paper");
     humanScore = humanScore + 1;
   } else if (humanChoice == "Rock" && computerChoice == "Scissors") {
-    console.log("You've Won! Rock beat Scissors");
-    computerScore = computerScore + 1;
+    console.log("You've Won! Rock beats Scissors");
+    humanScore = humanScore + 1;
   } else if (humanChoice == "Paper" && computerChoice == "Rock") {
-    console.log("You've Won! Paper beat Rock");
-    computerScore = computerScore + 1;
+    console.log("You've Won! Paper beats Rock");
+    humanScore = humanScore + 1;
   } else {
     console.log("It was a Tie! Play again");
   }
@@ -44,3 +43,28 @@ function playround(humanChoice, computerChoice) {
 
 const humanScore = 0;
 const computerScore = 0;
+
+function playGame() {
+  playRound();
+  console.log(
+    `The Scoreboard is: Computer ${computerScore} - YOU ${humanScore}`
+  );
+  playRound();
+  console.log(
+    `The Scoreboard is: Computer ${computerScore} - You ${humanScore}`
+  );
+  playRound();
+  console.log(
+    `The Scoreboard is: Computer ${computerScore}- You ${humanScore}`
+  );
+  playRound();
+  console.log(
+    `The Scoreboard is: Computer ${computerScore} - You ${humanScore}`
+  );
+  playRound();
+  console.log(
+    `The Scoreboard is: Computer ${computerScore} - You ${humanScore}`
+  );
+}
+
+playGame();
